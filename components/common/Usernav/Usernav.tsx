28f0306ac986018a028f83a529d63051de84e989
Necip <nexco@hotmail.com>
2021-09-20 04:09:50 +0300

    Sidebar, CartSiderbar, Usernav ve context api eklemeleri yapıldı

---
+++ components/common/Usernav/Usernav.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react'
+import Link from 'next/link'
+import s from './Usernav.module.css'
+import { Heart, Bag as Cart } from '../../icons/'
+import { useUI } from '@components/ui/context'
+
+const Usernav: FC = () => {
+  const { openSidebar } = useUI()
+  return (
+    <nav>
+      <ul className={s.list}>
+        <li className={s.item}>
+          <Cart onClick={openSidebar} />
+        </li>
+        <li className={s.item}>
+          <Link href='/wishlist'>
+            <a>
+              <Heart />
+            </a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Usernav
